996e95857b38476bcde3ef99a3e20e2d1bf8ada8
suryaa6666 <suryaelidanto@gmail.com>
2023-07-12 13:09:31 +0700

    day-8

---
+++ day8/exercise/1.customHof.js
@@ -0,0 +1,22 @@
+// // Define a function that takes a function as an argument.
+// function getCapture(test) {
+//     // Invoke the passed function
+//     test("surya");
+// }
+
+// // Invoke the function by passing a function as an argument
+// getCapture(function (nama) {
+//     console.log("halo", nama);
+// });
+
+// function returnFunc() {
+//     return function (nama) {
+//         return "hello " + nama
+//     }
+// }
+
+// console.log(returnFunc()("zidane"))
+
+// let a = function () { return "hello" }
+
+// console.log(a())
---
+++ day8/exercise/3.builtInHof.js
@@ -0,0 +1,36 @@
+// forEach -> loop tidak me-return data
+// const data1 = [5, 4, 3, 2, 1]
+
+// data1.forEach((item, index) => {
+//     // console.log("item : ", item)
+//     // console.log("index : ", index)
+// })
+
+// console.log(double)
+
+// map -> loop tp dia me-return data
+// const data2 = [1, 2, 3, 4, 5]
+
+// const double = data2.map(function (value, index) {
+//     return value * 2
+// })
+
+// console.log(double)
+
+// filter -> bakal pake buat bikin sistem rating
+// const data3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const filteredData = data3.filter((value, index) => value % 2 == 1);
+
+// console.log(filteredData)
+
+// reduce -> previous value, currentvalue
+// const data4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const sumData = data4.reduce((prev, value) => {
+//     return prev * value
+// })
+
+// console.log(sumData)
+
+
